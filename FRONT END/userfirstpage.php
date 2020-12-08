0b80f4028a700b8e601cbf9634397ca11b3f33d9
rohith cheryala <cheryalarohith@gmail.com>
2020-12-08 10:45:21 +0530

    init #7 userfirstpage.php

---
+++ FRONT END/userfirstpage.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link rel="stylesheet" href="style.css">
+<?php
+
+ob_start();
+session_start();
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$db="bssdb";
+
+$conn = mysqli_connect($servername, $username, $password, $db);
+?>
+</head>
+<body>
+    <div class="fix">
+    <div class="header">
+        <div class="log">
+            <img src="logo_2.png" width=80px height=70px>
+        </div>
+        <div class="left">
+            <h1>BUS RESERVATION SYSTEM<sup>TM</sup></h1>
+        </div>
+    </div>
+    <div class="topnav">
+        <a class="active">Schedule</a>
+        <a href="user">Users</a>
+        <a href=#employeepage>Employees</a>
+        <a href="accounts">Accounts</a>
+        <a href="transactions">Transactions</a>
+        <a class="logout">logout</a>
+    </div>
+    </div>
+    <br><br><br><br><br><br><br><br><br>
+    <div class="floleft">
+        
+    </div>
+    </body>
+</html>
